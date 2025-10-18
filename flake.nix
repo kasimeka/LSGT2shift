30fb290455cdb3a9ea68a8cd2f7f6f6d027ddc8a
ورد <kasimeka@tuta.io>
2025-10-18 03:47:45 +0300

    remove flake-utils

--- flake.nix
+++ flake.nix
@@ -2,16 +2,22 @@
   nixConfig.bash-prompt-prefix = ''(LSGT2shift) '';
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
-      stdenv = pkgs.pkgsMusl.stdenv;
+  outputs = inputs: let
+    outputs = inputs.self.outputs;
 
-      pname = "lsgt2shift";
-      drv = stdenv.mkDerivation {
+    forAllSystems = f:
+      inputs.nixpkgs.lib.genAttrs
+      (import inputs.systems)
+      (system: f inputs.nixpkgs.legacyPackages.${system});
+
+    pname = "lsgt2shift";
+
+    mkStdenv = pkgs: pkgs.pkgsMusl.stdenv;
+    mkDrv = pkgs:
+      (mkStdenv pkgs).mkDerivation {
         inherit pname;
         version = "v1.0.0";
 
@@ -29,48 +35,52 @@
 
         meta.mainProgram = pname;
       };
-    in rec {
-      devShells.default = (pkgs.mkShell.override {inherit stdenv;}) {
-        inputsFrom = [drv];
+  in rec {
+    devShells = forAllSystems (pkgs: {
+      default = (pkgs.mkShell.override {stdenv = mkStdenv pkgs;}) {
+        inputsFrom = [outputs.packages.${pkgs.system}.default];
         packages = [pkgs.clang-tools];
       };
+    });
 
-      packages.default = packages.${pname};
-      packages.${pname} = drv;
+    packages = forAllSystems (pkgs: {
+      default = outputs.packages.${pkgs.system}.${pname};
+      ${pname} = mkDrv pkgs;
+    });
 
-      overlays.default = overlays.${pname};
-      overlays.${pname} = _: prev: {
-        interception-tools-plugins =
-          prev.interception-tools-plugins // {${pname} = packages.default;};
-      };
+    overlays.default = overlays.${pname};
+    overlays.${pname} = _: prev: {
+      interception-tools-plugins =
+        prev.interception-tools-plugins // {${pname} = outputs.packages.${prev.system}.default;};
+    };
 
-      nixosModules.default = nixosModules.${pname};
-      nixosModules.${pname} = {
-        config,
-        pkgs,
-        lib,
-        ...
-      }: {
-        options.janw4ld.lsgt2shift.enable =
-          lib.mkEnableOption "enable lsgt2shift interception-tools plugin";
+    nixosModules.default = nixosModules.${pname};
+    nixosModules.${pname} = {
+      config,
+      pkgs,
+      lib,
+      ...
+    }: {
+      options.janw4ld.lsgt2shift.enable =
+        lib.mkEnableOption "enable lsgt2shift interception-tools plugin";
 
-        config = lib.mkIf config.janw4ld.lsgt2shift.enable {
-          nixpkgs.overlays = [overlays.default];
-          services.interception-tools = {
-            enable = lib.mkDefault true;
-            udevmonConfig = with pkgs; let
-              intercept = lib.getExe' interception-tools "intercept";
-              uinput = lib.getExe' interception-tools "uinput";
-              lsgt2shift = lib.getExe pkgs.interception-tools-plugins.${pname};
-            in
-              lib.mkDefault (builtins.toJSON [
-                {
-                  JOB = "${intercept} -g $DEVNODE | ${lsgt2shift} | ${uinput} -d $DEVNODE";
-                  DEVICE.EVENTS.EV_KEY = ["KEY_102ND"];
-                }
-              ]);
-          };
+      config = lib.mkIf config.janw4ld.lsgt2shift.enable {
+        nixpkgs.overlays = [outputs.overlays.default];
+        services.interception-tools = {
+          enable = lib.mkDefault true;
+          udevmonConfig = with pkgs; let
+            intercept = lib.getExe' interception-tools "intercept";
+            uinput = lib.getExe' interception-tools "uinput";
+            lsgt2shift = lib.getExe pkgs.interception-tools-plugins.${pname};
+          in
+            lib.mkDefault (builtins.toJSON [
+              {
+                JOB = "${intercept} -g $DEVNODE | ${lsgt2shift} | ${uinput} -d $DEVNODE";
+                DEVICE.EVENTS.EV_KEY = ["KEY_102ND"];
+              }
+            ]);
         };
       };
-    });
+    };
+  };
 }
